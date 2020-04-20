b64125c9b83bfdbe1c1a3fca26c1a43ded3975b4
J-Beekman <46580879+J-Beekman@users.noreply.github.com>
2020-04-20 11:02:52 -0700

    Initial commit

---
+++ AppointmentRegistration/Classes/Strings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentRegistration
+{
+    public partial class Patient
+    {
+        public string resultsString => $"{Id} {Name} {address} {phoneNumber}";
+        public override string ToString()
+        {
+            return resultsString;
+        }
+    }
+
+    public partial class Schedule
+    {
+        public string resultsString => $"{appointmentId} {Patient.Name} {Doctor.name} {Nurse.name}";
+    }
+}
---
+++ AppointmentRegistration/Forms/MainScreen.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+using AppointmentRegistration.Forms;
+
+namespace AppointmentRegistration
+{ 
+    public partial class MainScreen : Form
+    {
+        private string operation = "";
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void hideTopButtons()
+        {
+            createButton.Visible = false;
+            readButton.Visible = false;
+            updateButton.Visible = false;
+            deleteButton.Visible = false;
+            patientButton.Visible = true;
+            doctorButton.Visible = true;
+            nurseButton.Visible = true;
+            appointmentButton.Visible = true;
+            scheduleButton.Visible = true;
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            operation = "create";
+            hideTopButtons();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            operation = "read";
+            hideTopButtons();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            operation = "update";
+            hideTopButtons();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            operation = "delete";
+            hideTopButtons();
+        }
+
+        private void patientButton_Click(object sender, EventArgs e)
+        {
+            PatientForm patientForm = new PatientForm(operation);
+            patientForm.Show();
+            this.Hide();
+        }
+
+        private void doctorButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nurseButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appointmentButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scheduleButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
